a72d41f8d851384363e66264b82e3c10e619c523
Filipe Moraes <filipemoraes@MacBook-Pro.local>
2025-08-02 22:46:11 +0100

    feat(rewrite): allows mapping the request for a static file without exposing the Strapi admin URL

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,26 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/uploads/:path*',
+        destination: `${process.env.API_BASE_URL}/uploads/:path*`,
+      },
+    ];
+  },
+  images: {
+    qualities: [100],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+        pathname: '/uploads/**',
+      }
+    ],
+  },
+};
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
