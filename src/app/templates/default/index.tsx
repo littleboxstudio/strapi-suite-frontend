9cdb1ccb5b1dcd21c6b8e16257bf1abefd1de4c1
Filipe Moraes <filipemoraes87@gmail.com>
2025-10-09 20:50:47 +0100

    fix(templates): fixes the documentation link

--- src/app/templates/default/index.tsx
+++ src/app/templates/default/index.tsx
@@ -48,7 +48,7 @@ export default function Default({ page }: Props) {
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://strapi.littlebox.pt"
+            href="https://strapi-suite.littlebox.pt"
             target="_blank"
             rel="noopener noreferrer"
           >
